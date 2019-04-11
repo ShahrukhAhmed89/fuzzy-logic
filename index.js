87e5acce5c8f570ece3cb8a7f5ee3b66e8eba0be
Shahrukh  Ahmed <shahrukh.gunnar@gmail.com>
2019-04-11 21:58:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 var app = new Vue({
     el: '#container',
+    // data(){
+    //     return {
+    //         userSize: {
+    //             shoulder: 45,
+    //             chest: 106,
+    //             waist: 49,
+    //         },
+    //         brandMeasurement:{
+    //             shoulder: [42, 44, 46, 48],
+    //             chest:    [98, 104, 110, 114],
+    //             waist:    [46, 52, 54, 56]
+    //         },
+    //         recommendedSize: ""
+    //     }
+    // },
     data: {
-      userSize: {
-          collar: 40,
-          shoulder: 44,
-          chest: 100,
-          waist: 48,
-      },
-      brandMeasurement:{
-          collar:   [38, 40, 42, 44],
-          shoulder: [42, 44, 46, 48],
-          chest:    [98, 104, 110, 114],
-          waist:    [46, 52, 54, 56]
-      },
-      recommendedSize: ""
+        userSize: {
+            shoulder: 45,
+            chest: 106,
+            waist: 49,
+        },
+        brandMeasurement:{
+            shoulder: [42, 44, 46, 48],
+            chest:    [98, 104, 110, 114],
+            waist:    [46, 52, 54, 56]
+        },
+        recommendedSize: ""
+    },
+    watch:{
+        shoulder: function (newVal, oldVal){
+            console.log(newval)
+            this.calculateSize()
+        },
+        deep: true
     },
     methods: {
-        calculateCollar: function(){
-            let lowerLimitCollar = this.userSize.collar*.85
+        calculateSize: function(){
             let lowerLimitShoulder = this.userSize.shoulder*.85
             let lowerLimitChest = this.userSize.chest*.85
             let lowerLimitWaist = this.userSize.waist*.85
 
 
             parameterS = {
-               collar:{
-                    a: lowerLimitCollar, 
-                    b: this.brandMeasurement.collar[0]*.965,
-                    c: this.brandMeasurement.collar[0]
-               },
-               shoulder:{
+                shoulder:{
                     a: lowerLimitShoulder, 
                     b: this.brandMeasurement.shoulder[0]*.965,
                     c: this.brandMeasurement.shoulder[0]
@@ -47,11 +61,6 @@ var app = new Vue({
             }
 
             parameterM = {
-                collar:{
-                     a: lowerLimitCollar, 
-                     b: this.brandMeasurement.collar[1]*.965,
-                     c: this.brandMeasurement.collar[1]
-                },
                 shoulder:{
                      a: lowerLimitShoulder, 
                      b: this.brandMeasurement.shoulder[1]*.965,
@@ -69,12 +78,8 @@ var app = new Vue({
                 }
             }
 
+            
             parameterL = {
-                collar:{
-                     a: lowerLimitCollar, 
-                     b: this.brandMeasurement.collar[2]*.965,
-                     c: this.brandMeasurement.collar[2]
-                },
                 shoulder:{
                      a: lowerLimitShoulder, 
                      b: this.brandMeasurement.shoulder[2]*.965,
@@ -93,11 +98,6 @@ var app = new Vue({
             }
 
             parameterXL = {
-                collar:{
-                     a: lowerLimitCollar, 
-                     b: this.brandMeasurement.collar[3]*.965,
-                     c: this.brandMeasurement.collar[3]
-                },
                 shoulder:{
                      a: lowerLimitShoulder, 
                      b: this.brandMeasurement.shoulder[3]*.965,
@@ -114,7 +114,7 @@ var app = new Vue({
                      c: this.brandMeasurement.waist[3]
                 }
             }
-            
+
             let mu = {
                 small: this.degreeOfFitness(parameterS),
                 medium: this.degreeOfFitness(parameterM),
@@ -124,12 +124,14 @@ var app = new Vue({
             this.recommendedSize =  Object.keys(mu).reduce((a, b) => {
                return mu[a] > mu[b] ? a : b;
             });
-            console.log(this.recommendedSize)
         },
         getMemberShipValue: function(x, a, b, c){
+            let val = 0
             if ( a < x && x <= b ){
+                val = Number(((x-a)/(b-a)).toFixed(3))
                 return Number(((x-a)/(b-a)).toFixed(3))
             }else if(b < x && x <= c){
+                val = Number(((c-x)/(c-b)).toFixed(3))
                 return Number(((c-x)/(c-b)).toFixed(3))
             }else{
                 return 0
@@ -138,14 +140,174 @@ var app = new Vue({
         degreeOfFitness: function(param){
             let muArray = []
             for (i in param){
+                let test = this.getMemberShipValue(this.userSize[i], ...Object.values(param[i]))
+                // console.log(this.userSize[i], ...Object.values(param[i]), test)
                 muArray.push(this.getMemberShipValue(this.userSize[i], ...Object.values(param[i])))
             }
             return (Math.min(...muArray))
 
         }
+    },
+    mounted(){
+        this.calculateSize()
     }
   })
 
+
+
+//   var app = new Vue({
+//     el: '#container',
+//     data: {
+//       userSize: {
+//           collar: 39,
+//           shoulder: 45,
+//           chest: 106,
+//           waist: 49,
+//       },
+//       brandMeasurement:{
+//           collar:   [38, 40, 42, 44],
+//           shoulder: [42, 44, 46, 48],
+//           chest:    [98, 104, 110, 114],
+//           waist:    [46, 52, 54, 56]
+//       },
+//       recommendedSize: ""
+//     },
+//     methods: {
+//         calculateCollar: function(){
+//             let lowerLimitCollar = this.userSize.collar*.85
+//             let lowerLimitShoulder = this.userSize.shoulder*.85
+//             let lowerLimitChest = this.userSize.chest*.85
+//             let lowerLimitWaist = this.userSize.waist*.85
+
+
+//             parameterS = {
+//                collar:{
+//                     a: lowerLimitCollar, 
+//                     b: this.brandMeasurement.collar[0]*.965,
+//                     c: this.brandMeasurement.collar[0]
+//                },
+//                shoulder:{
+//                     a: lowerLimitShoulder, 
+//                     b: this.brandMeasurement.shoulder[0]*.965,
+//                     c: this.brandMeasurement.shoulder[0]
+//                },
+//                chest:{
+//                     a: lowerLimitChest, 
+//                     b: this.brandMeasurement.chest[0]*.965,
+//                     c: this.brandMeasurement.chest[0]
+//                },
+//                waist:{
+//                     a: lowerLimitWaist, 
+//                     b: this.brandMeasurement.waist[0]*.965,
+//                     c: this.brandMeasurement.waist[0]
+//                }
+//             }
+
+//             parameterM = {
+//                 collar:{
+//                      a: lowerLimitCollar, 
+//                      b: this.brandMeasurement.collar[1]*.965,
+//                      c: this.brandMeasurement.collar[1]
+//                 },
+//                 shoulder:{
+//                      a: lowerLimitShoulder, 
+//                      b: this.brandMeasurement.shoulder[1]*.965,
+//                      c: this.brandMeasurement.shoulder[1]
+//                 },
+//                 chest:{
+//                      a: lowerLimitChest, 
+//                      b: this.brandMeasurement.chest[1]*.965,
+//                      c: this.brandMeasurement.chest[1]
+//                 },
+//                 waist:{
+//                      a: lowerLimitWaist, 
+//                      b: this.brandMeasurement.waist[1]*.965,
+//                      c: this.brandMeasurement.waist[1]
+//                 }
+//             }
+
+            
+//             parameterL = {
+//                 collar:{
+//                      a: lowerLimitCollar, 
+//                      b: this.brandMeasurement.collar[2]*.965,
+//                      c: this.brandMeasurement.collar[2]
+//                 },
+//                 shoulder:{
+//                      a: lowerLimitShoulder, 
+//                      b: this.brandMeasurement.shoulder[2]*.965,
+//                      c: this.brandMeasurement.shoulder[2]
+//                 },
+//                 chest:{
+//                      a: lowerLimitChest, 
+//                      b: this.brandMeasurement.chest[2]*.965,
+//                      c: this.brandMeasurement.chest[2]
+//                 },
+//                 waist:{
+//                      a: lowerLimitWaist, 
+//                      b: this.brandMeasurement.waist[2]*.965,
+//                      c: this.brandMeasurement.waist[2]
+//                 }
+//             }
+
+//             parameterXL = {
+//                 collar:{
+//                      a: lowerLimitCollar, 
+//                      b: this.brandMeasurement.collar[3]*.965,
+//                      c: this.brandMeasurement.collar[3]
+//                 },
+//                 shoulder:{
+//                      a: lowerLimitShoulder, 
+//                      b: this.brandMeasurement.shoulder[3]*.965,
+//                      c: this.brandMeasurement.shoulder[3]
+//                 },
+//                 chest:{
+//                      a: lowerLimitChest, 
+//                      b: this.brandMeasurement.chest[3]*.965,
+//                      c: this.brandMeasurement.chest[3]
+//                 },
+//                 waist:{
+//                      a: lowerLimitWaist, 
+//                      b: this.brandMeasurement.waist[3]*.965,
+//                      c: this.brandMeasurement.waist[3]
+//                 }
+//             }
+
+//             let mu = {
+//                 small: this.degreeOfFitness(parameterS),
+//                 medium: this.degreeOfFitness(parameterM),
+//                 large:this.degreeOfFitness(parameterL),
+//                 xLarge: this.degreeOfFitness(parameterXL),
+//             }
+//             this.recommendedSize =  Object.keys(mu).reduce((a, b) => {
+//                return mu[a] > mu[b] ? a : b;
+//             });
+//         },
+//         getMemberShipValue: function(x, a, b, c){
+//             let val = 0
+//             if ( a < x && x <= b ){
+//                 val = Number(((x-a)/(b-a)).toFixed(3))
+//                 return Number(((x-a)/(b-a)).toFixed(3))
+//             }else if(b < x && x <= c){
+//                 val = Number(((c-x)/(c-b)).toFixed(3))
+//                 return Number(((c-x)/(c-b)).toFixed(3))
+//             }else{
+//                 return 0
+//             }
+//         },
+//         degreeOfFitness: function(param){
+//             let muArray = []
+//             for (i in param){
+//                 let test = this.getMemberShipValue(this.userSize[i], ...Object.values(param[i]))
+//                 debugger;
+//                 console.log(this.userSize[i], ...Object.values(param[i]), test)
+//                 muArray.push(this.getMemberShipValue(this.userSize[i], ...Object.values(param[i])))
+//             }
+//             return (Math.min(...muArray))
+
+//         }
+//     }
+//   })
 
 // let parameter = {
 //     small: {
